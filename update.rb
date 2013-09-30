fd1f3574dd3be068082fbbc2a4423ca345d73f29
Liam Anderson <Liam.Anderson.91@gmail.com>
2013-09-30 13:14:14 +0100

    Ruby script to update "last modified" date and time.

---
+++ update.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+require "FileUtils"
+
+pretext = "! Last modified: "
+timestamp = Time.new.gmtime.strftime("%e %B %Y %R %Z")
+original_file = "obtrusive.txt"
+temp_file = original_file + ".tmp"
+
+FileUtils.mv original_file, temp_file
+
+File.open(temp_file, "r") { |file|
+	text = Array.new
+	file.each_line { |line|
+		if line.include? pretext then line = pretext + timestamp + "\n" end
+		text.push line
+	}
+	File.open(original_file, "w") { |file|
+		file.write text.join.to_s
+	}
+}
+
+FileUtils.rm temp_file
